c62a168fed9b499907a4882d2d88143c968edbf7
singhprince123 <princersingh696@gmail.com>
2019-02-10 16:14:43 +0530

    first commit

---
+++ model/event.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const eventSchema = new schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: schema.Types.ObjectId,
+        ref: 'User'
+    }
+
+});
+
+module.exports = mongoose.model('Event', eventSchema);
