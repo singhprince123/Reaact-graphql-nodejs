5e9e80473cef8fe1d348cd3ccd6c12d24716bb7c
singhprince123 <princersingh696@gmail.com>
2019-02-13 01:35:15 +0530

    added login feature

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql')
-const mongoose  = require('mongoose')
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const mongoose = require("mongoose");
 
-const graphqlSchema = require('./graphql/schema/index')
-const graphqlResover = require('./graphql/resolver/index')
-const isAuth = require('./middleware/is-auth')
+const graphqlSchema = require("./graphql/schema/index");
+const graphqlResover = require("./graphql/resolver/index");
+const isAuth = require("./middleware/is-auth");
 
 const app = express();
 
-
-
 app.use(bodyParser.json());
 
-app.use(isAuth)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-app.get('/', (req, res, next)=> {
-  res.send('hello world!')
-})
+app.use(isAuth);
 
+app.get("/", (req, res, next) => {
+  res.send("hello world!");
+});
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResover,
     graphiql: true
-}))
+  })
+);
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-4fp9q.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
-  .then(()=> { console.log('connected to mongodb atlas..')})
-  .catch(err => console.log(err))
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-4fp9q.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("connected to mongodb atlas..");
+  })
+  .catch(err => console.log(err));
 
-
-app.listen(5000,() => console.log('server started at : 5000'))
+app.listen(5000, () => console.log("server started at : 5000"));
