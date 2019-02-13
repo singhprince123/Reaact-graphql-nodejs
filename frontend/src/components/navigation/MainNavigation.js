27dd50d07b62fc5cc1bd3e2a08708db0270c0b4e
singhprince123 <princersingh696@gmail.com>
2019-02-13 19:55:10 +0530

    added logout

--- frontend/src/components/navigation/MainNavigation.js
+++ frontend/src/components/navigation/MainNavigation.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './MainNavigation.css'
+import AuthContext from '../../context/auth-context'
 
 export default function MainNavigation(props) {
   return (
-    <header className="main-navigation">
+    <AuthContext.Consumer>
+    { (context) => {
+      
+      return (
+        <header className="main-navigation">
         <div className="main-navigation__logo">
           <h1>EasyEvent</h1>
         </div>
         <div className="main-navigation__items">
-           <ul>
-               <li><NavLink to="/auth">Authenticate</NavLink> </li>
+          <ul> {!context.token &&  <li><NavLink to="/auth">Authenticate</NavLink> </li> }
+              
                <li><NavLink to="/events">Events</NavLink> </li>
+
+               {context.token && 
+                <React.Fragment>
                <li><NavLink to="/bookings">Bookings</NavLink> </li>
-               
+                <li><button onClick={context.logout}>Logout</button></li>
+                </React.Fragment>
+               }
            </ul>
         </div>
     </header>
+      )
+    }}
+  </AuthContext.Consumer>
   )
+ 
+
 }
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,126 @@
 import React, { Component } from 'react'
+import './Events.css'
+import Modal from '../components/Modal/Modal'
+import BackDrop from '../components/Backdrop/Backdrop'
+import AuthContext from '../context/auth-context';
 
 export default class Auth extends Component {
+  state={
+    creating: false
+  }
+
+  static contextType = AuthContext;
+
+  constructor(props){
+    super(props)
+    this.titleElRef = React.createRef()
+    this.priceElRef = React.createRef()
+    this.dateElRef = React.createRef()
+    this.descriptionElRef = React.createRef()
+  }
+
+  createEvetnHandler = ()=>{
+    this.setState({
+      creating: true
+    })
+  }
+
+  modalConfirm = ()=>{
+   
+    console.log("in modal confirm")
+    const title = this.titleElRef.current.value;
+    const price = +this.priceElRef.current.value;
+    const date  = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+    if(title.trim().length ===0 || price <= 0 || date.trim().length === 0 || description.trim().length === 0){
+      console.log( "in if")
+      return ;
+    }
+    const event = {title, price, date, description};
+    console.log(event)
+
+    const requsetBody  = {
+      query : `
+        mutation {
+          createEvent(eventInput : { title: "${title}", description: "${description}", price: ${price}, date: "${date}"}){
+             _id
+             title
+             description
+             price
+             date
+             creator {
+               _id
+               email
+             }
+          }
+        }
+      `
+    };
+
+    const token = this.contextType.token;
+    console.log("token =", token)
+
+    fetch('http://localhost:5000/graphql',{
+      method:'POST',
+      body: JSON.stringify(requsetBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    }).then(res => {
+      if(res.status !== 200 && res.status !== 201){
+        throw new Error('Failed');
+      }
+      return res.json()
+    }).then(resData => {
+      console.log("resdata =",resData)
+    }).catch(err => console.log(err))
+
+    this.setState({
+      creating: false
+    });
+  }
+
+  modalCancel = () => {
+    this.setState({
+      creating: false
+    })
+  }
+
   render() {
     return (
-      <h1>hello form events.js</h1>
+      <React.Fragment>
+        {this.state.creating && <BackDrop />}
+        {this.state.creating && <Modal canCancel title="Add Event" canConfirm
+        onConfirm={this.modalConfirm}
+        onCancel={this.modalCancel}
+        >
+        <form>
+        <div className="form-control">
+          <label htmlFor="title">Title</label>
+           <input type="text" id="title" ref={this.titleElRef}/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="price">Price</label>
+           <input type="number" id="price" ref={this.priceElRef} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="date">Date</label>
+           <input type="datetime-local" id="date" ref={this.dateElRef}/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="description">Description</label>
+           <textarea rows="5" id="description" ref={this.descriptionElRef} />
+        </div>
+        </form>
+        
+      </Modal>}
+       
+      <div className="events-control">
+        <p>Share Your Events</p>
+        <button className="btn" onClick={this.createEvetnHandler}>create event</button>
+      </div>
+      </React.Fragment>
     )
   }
 }
